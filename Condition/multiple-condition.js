022159a8e525073424ee6d5bdc6300c4de3b33b0
monoronjon <monoronjons86@gmail.com>
2024-03-04 13:52:22 +0600

    multiple condition

---
+++ Condition/multiple-condition.js
@@ -0,0 +1,11 @@
+var isGraduated=true;
+var salary=55000;
+var cars=1;
+
+if(isGraduated ==true && salary>50000 && cars>=1){
+    console.log('Eso biya kore ');
+}
+else{
+    console.log('Tor kopal a biya nai')
+}
+
