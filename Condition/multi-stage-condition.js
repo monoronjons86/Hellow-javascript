f200917e7768b3506309cc13a76a21c33ff263ef
monoronjon <monoronjons86@gmail.com>
2024-03-04 16:49:16 +0600

    today javascript end

---
+++ Condition/multi-stage-condition.js
@@ -0,0 +1,16 @@
+var money=10;
+var danishPrice=45;
+var butterBread=35;
+var toastBiscuit=20;
+if(danishPrice<money){
+    console.log('Dan Dan danish khamu')
+}
+else if(butterBread<money){
+    console.log('buttor bon khamu')
+}
+else if(toastBiscuit<money){
+    console.log('tostBiscuit khamu')
+}
+else{
+    console.log('coffe dan khamu')
+}
