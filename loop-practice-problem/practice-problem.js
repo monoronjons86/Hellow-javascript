dc4256672f6c6b6dc481c75dead1ca40f49c2df3
monoronjon <monoronjons86@gmail.com>
2024-03-05 12:12:46 +0600

    simple

---
+++ loop-practice-problem/practice-problem.js
@@ -0,0 +1,19 @@
+// var moneyGiven=1000;
+// var applePrice=400;
+// var orangePrice=300;
+
+// var totalPrice=applePrice+orangePrice
+// var getBack=moneyGiven-totalPrice;
+// console.log(getBack)
+
+var mathMarks=75.25;
+var biologyMarks=65;
+var chemistyMarks=35.45;
+var phycisMarks=34.2;
+var banglaMarks=89;
+
+var totalmarks=mathMarks+biologyMarks+chemistyMarks+banglaMarks;
+console.log(totalmarks);
+var average=totalmarks/4
+console.log(average);
+
