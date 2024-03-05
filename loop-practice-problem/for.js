62928dd12be0238aa8d8ddbd6e55995674750138
monoronjon <monoronjons86@gmail.com>
2024-03-05 09:45:46 +0600

    for add

---
+++ loop-practice-problem/for.js
@@ -0,0 +1,10 @@
+
+for(var rostGiven=0;rostGiven<7;rostGiven++){
+    console.log('Rost,den please!!');
+}
+
+// shortversion
+console.log('simple for loop')
+for(var i=0;i<7; i++){
+    console.log(i);
+}
