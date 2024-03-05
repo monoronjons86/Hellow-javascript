8a36a093d8b2090a860ed905578c1de4635f8478
monoronjon <monoronjons86@gmail.com>
2024-03-05 08:39:58 +0600

    while end

---
+++ loop-practice-problem/number.js
@@ -0,0 +1,12 @@
+// var number=1;
+// while(number<=10){
+//     console.log(number)
+//     number++;
+// }
+
+var number=0;
+while(number<=20){
+    console.log(number);
+    number=number+2;
+}
+
