92a28907b57a8819400e83cfe56c03e0ef6070eb
monoronjon <monoronjons86@gmail.com>
2024-03-05 11:12:29 +0600

    break-end

---
+++ loop-practice-problem/break.js
@@ -0,0 +1,15 @@
+// for(var i=1;i<=20; i++){
+//     console.log(i);
+//     if(i>10){
+//         break;
+//     }
+// }
+
+var roastGiven=0;
+while(roastGiven<10){
+    console.log('Roast Den,gift item anci');
+    roastGiven++;
+    if(roastGiven>4){
+        break;
+    }
+}
