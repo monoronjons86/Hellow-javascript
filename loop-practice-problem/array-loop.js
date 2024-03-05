6a4b13fea160c4907b6aa473818632a85dd6a968
monoronjon <monoronjons86@gmail.com>
2024-03-05 10:22:40 +0600

    string array for

---
+++ loop-practice-problem/array-loop.js
@@ -0,0 +1,11 @@
+var numbers=[45,87,89,56,32,51,25]
+for(var i=0;i<7;i++){
+    var number=numbers[i]
+    console.log(number)
+}
+
+const items=['apple','Orange','mango','bannana']
+for(var i=0;i<items.length;i++){
+    var item=items[i]
+    console.log(item)
+}
