a5799a27b69f3df8b4e33c5d12578bc9f787d1ae
monoronjon <monoronjons86@gmail.com>
2024-03-07 00:14:59 +0600

    object destrat

---
+++ Es6Examp/destructure.js
@@ -0,0 +1,9 @@
+const actor={
+    name:'Ananata',
+    age:30,
+    phone:'0188524579',
+    money:123432
+}
+
+const {age}=actor;
+console.log(age);
---
+++ Es6Examp/spreed.js
@@ -0,0 +1,5 @@
+const max=Math.max(6,23,24,25,27,28)
+console.log(max);
+
+const numbers=[3,5,2,45,5,43,90,32,15]
+
