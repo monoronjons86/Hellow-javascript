75b4dcc37d327287ac61b6add23b5d3312fd7405
monoronjon <monoronjons86@gmail.com>
2024-03-06 21:25:52 +0600

    template add

---
+++ Es6Examp/template.js
@@ -0,0 +1,17 @@
+const first='udashi'
+const last='last'
+
+const name1=first+last;
+console.log(name1)
+
+const a=20;
+const b=30;
+const math=`The sum of ${a} and ${b} is ${a+b}`
+console.log(math);
+
+const challenge=`
+hellow mono
+how are you
+what ar you doing
+`
+console.log(challenge)
