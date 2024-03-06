847ff9d5876bcd742890065aaff9d533e3a3205a
monoronjon <monoronjons86@gmail.com>
2024-03-06 11:18:38 +0600

    function end

---
+++ functin-objct/functin.js
@@ -0,0 +1,17 @@
+//function declartion
+// function startFan(){
+//     console.log('Stand up');
+//     console.log('walk towards the switch')
+//     console.log('press the switch')
+// }
+// //call the function
+// startFan();
+
+function add(number1,number2){
+    // console.log(number1,number2);
+    var sum=number1+number2;
+    return sum;
+}
+var total=add(80,20);
+console.log(total);
+
