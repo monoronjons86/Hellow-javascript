3a644a95e84291094c4abad0e71949a529c57e6f
monoronjon <monoronjons86@gmail.com>
2024-03-06 17:49:48 +0600

    object-end

---
+++ functin-objct/object.js
@@ -0,0 +1,19 @@
+var student={
+    id:115,
+    name:'solamin khan',
+    class:9,
+    marks:98
+}
+var mobile={
+    brand:'samsung',
+    price:1900,
+    storage:'64gb',
+    camera:'7MP'
+}
+
+var myComputer={
+    brand:'lonovo',
+    price:3900,
+    color:'silver'
+}
+console.log(myComputer)
