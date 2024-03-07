cd761eff23591b5cd019f79692abdfa56630e30f
monoronjon <monoronjons86@gmail.com>
2024-03-07 23:29:06 +0600

    type add

---
+++ Es6Examp/type.js
@@ -0,0 +1,10 @@
+const a=5;
+const b='Samsu kopai na akhn ar ';
+const d=true;
+const ages=[45,65,48]
+const student={id:23,class:7}
+// console.log(typeof a,typeof b,typeof ages, typeof student)
+
+let x=5;
+let y=x;
+console.log(x,y)
