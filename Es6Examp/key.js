d6d7d170d45181982836375c05b7029fe8bad2bc
monoronjon <monoronjons86@gmail.com>
2024-03-07 09:00:53 +0600

    key end

---
+++ Es6Examp/key.js
@@ -0,0 +1,6 @@
+const glass={name:'glass',color:'golden',price:12,isCleane:true};
+console.log(glass);
+// const key=object.keys(glass)
+// console.log(key);
+const values=Object.values(glass)
+console.log(values)
