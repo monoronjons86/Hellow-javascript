bd4252f41e8592a1ff063824331a8b860db1fe3c
monoronjon <monoronjons86@gmail.com>
2024-03-07 17:17:26 +0600

    filter

---
+++ Es6Examp/filter.js
@@ -0,0 +1,8 @@
+const numbers=[1,5,6,4,15]
+const players=[75,65,67,72,55,59 ]
+const selected=players.filter(p=>p>70)
+console.log(selected);
+
+const player1=[75,65,67,72,55,59]
+const selected1=player1.find(player=>player>70)
+console.log(selected1);
