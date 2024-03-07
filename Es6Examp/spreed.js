ebc1c23096482c2329c1392a2eefdfca4dbc0e86
monoronjon <monoronjons86@gmail.com>
2024-03-07 08:12:07 +0600

    spreed add

--- Es6Examp/spreed.js
+++ Es6Examp/spreed.js
@@ -3,3 +3,8 @@ console.log(max);
 
 const numbers=[3,5,2,45,5,43,90,32,15]
 
+const numbersOne = [1, 2, 3];
+const numbersTwo = [4, 5, 6];
+const numbersCombined = [...numbersOne, ...numbersTwo];
+
+console.log(numbersCombined)
